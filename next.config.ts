3281b4ff8596384806cf99c090386562c0571478
JasonSYuen <js99168@icloud.com>
2025-12-04 19:49:15 -0800

    revert

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/sample-word-games' : '',
+  /* config options here */
 };
 
 export default nextConfig;
